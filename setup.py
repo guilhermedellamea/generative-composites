77b1808b7e3ef3a2538c06ae36648e947b249870
guilhermedellamea <guilherme.basso_della_mea@minesparis.psl.eu>
2025-06-26 10:30:12 +0200

    fix setup name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="gen_composites",
+    name="generative-composites",
     version="1.0",
     packages=find_packages(),  # includes core/, training/, etc.
 )
